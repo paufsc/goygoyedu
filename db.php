254f55ca745e2f0ef61f9fa6933f51108ada1641
Kemal Kanok <kemalkanok@gmail.com>
2014-07-02 09:42:47 +0300

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,6 @@ function db($sql,$data = array())
 	 $pdo = new PDO("mysql:host=localhost;dbname=goygoyedulogin;charset=utf8","root","");
 	 $sql = $pdo -> prepare($sql);
 	 $sql -> execute($data);
-     $obj = ($pdo);
 	 return $sql->fetchAll(PDO::FETCH_CLASS); 
 }
 // function db($sql,$data = array())
